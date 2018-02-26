246e8ee036dbd0116855adcb55de1f31f4b3c2c6
Infeltrace <36425199+Infeltrace@users.noreply.github.com>
2018-02-26 11:40:57 +0200

    Add files via upload
    Done

---
+++ Lesson 3/Exception-Parse-URL/Main.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import com.sun.jndi.toolkit.url.Uri;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            methodArithmeticException();
+        }catch (ArithmeticException e){
+            System.out.println("Произошла ошибка :)");
+            e.printStackTrace();
+        }
+        try{
+            someMethodForException(-1);
+        }catch (MyException e) {
+            System.out.println("Произошла ошибка :)");
+            e.printStackTrace();
+        }
+
+        methodOutOfBounds();
+        methodNullPointerExeption();
+        methodFormatExeption();
+
+        try {
+            System.out.println(ParseAnalogy.parseInt("534778"));
+        }catch (NumberFormatException e){
+            System.out.println("NumberFormatException");
+        }
+
+        try {
+            URL.dissection("param1=value1&param2=value2&param3=value3");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void methodOutOfBounds() {
+
+        int arr[] = new int[5];
+        try {
+            arr[6] = 6;
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Произошла ошибка :)");
+           e.printStackTrace();
+        }
+    }
+
+   private static void methodFormatExeption(){
+         try {
+             int temp = Integer.parseInt("dfhdgfh");
+         }catch (NumberFormatException e){
+             System.out.println("Произошла ошибка :)");
+             e.printStackTrace();
+         }
+    }
+
+   private static void methodNullPointerExeption(){
+        String string = null;
+        try {
+            string.getBytes();
+        }catch (NullPointerException e){
+            System.out.println("Произошла ошибка :)");
+            e.printStackTrace();
+        }
+    }
+
+    private static void methodArithmeticException() throws ArithmeticException{
+        System.out.println(Math.floorDiv(1, 0));
+    }
+
+    private static void someMethodForException(int number) throws MyException{
+
+        if(number >= 0) System.out.println("OK");
+        else throw new MyException();
+    }
+
+}
---
+++ Lesson 3/Exception-Parse-URL/URL.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class URL {
+
+    static public void dissection(String string)throws Exception{
+
+        Map<String,String> map = new HashMap<>();
+
+        for(String s:string.split("&")) {
+            if(s.matches("[A-Za-z0-9]+=[A-Za-z0-9]+")){
+                map.put(s.split("=")[0],s.split("=")[1]);
+            }else throw new Exception();
+            System.out.println(s);
+        }
+    }
+}
