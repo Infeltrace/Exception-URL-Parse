4157833e8ea5194a15367b649e97a5b1103cfd02
Infeltrace <36425199+Infeltrace@users.noreply.github.com>
2018-02-26 11:30:05 +0200

    Add files via upload
    Done

---
+++ Lesson 2/Figure/Circle.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Circle extends Rectangle {
+
+    private int radius;
+
+    public Circle(int width, int height){
+        super(height,width);
+    }
+
+    public Circle(int radius) {
+        super();
+        this.radius = radius;
+    }
+
+
+    @Override
+    public double getSquare() {
+        return Math.PI*radius*radius;
+    }
+}
---
+++ Lesson 2/Figure/ClassFigura.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public abstract class ClassFigura {
+
+    protected String name;
+    protected int height;
+    protected int width;
+
+    Scanner scanner;
+
+    public ClassFigura(String name){
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double getSquare();
+}
